359a5e23fbfa50d0e48df3cafed51c491d35f52e
Jason Cox <dev@jasoncarloscox.com>
2019-11-14 08:55:03 -0700

    initial commit

---
+++ chroot.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Script run on the new root.
+
+# variables
+if [ -z "$1" ]; then
+    echo "USAGE: ./chroot.sh [default username]"
+    exit 1
+fi
+USER="$1"
+
+# set timezone
+echo "Setting timezone"
+ln -sf /usr/share/zoneinfo/America/Denver /etc/localtime
+hwclock --systohc
+
+# set locale
+echo "Setting locale"
+vim +/#en_US\.UFT-8 -c "normal! x" -c wq
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# save keyboard layout
+echo "Saving keyboard layout"
+echo "KEYMAP=colemak" > /etc/vconsole.conf
+
+# configure network
+echo "Configuring network"
+echo "jason-desktop" > /etc/hostname
+echo "127.0.0.1        localhost" >> /etc/hosts
+echo "::1              localhost" >> /etc/hosts
+echo "127.0.1.1        jason-desktop.localdomain" >> /etc/hosts
+
+# set up bootloader
+echo "Installing bootloader"
+pacman -S grub efibootmgr intel-ucode
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# install NetworkManager to connect to Internet after reboot
+echo "Installing NetworkManager"
+pacman -S networkmanager
+systemctl enable NetworkManager.service
+
+# SDDM
+echo "Installing SDDM"
+pacman -S --no-confirm xorg sddm
+systemctl enable sddm.service
+
+# KDE Plasma
+echo "Installing KDE Plasma desktop environment"
+pacman -S --no-confirm phonon-qt5-vlc plasma-meta plasma-nm sddm-kcm kde-gtk-config libdbusmenu-glib libdbusmenu-gtk2 libdbusmenu-gtk3 kdeconnect
+
+# Other Packages
+echo "Installing additional software"
+pacman -S --no-confirm base-devel git konsole firefox gvim zip unzip openssh code hunspell-en_US hunspell-es_any nextcloud-client yakuake pulseaudio-alsa pulseaudio-bluetooth
+
+# Yay (AUR helper)
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+# AUR Packages
+yay -S --no-confirm tutanota-desktop-linux
+
+# Keyboard
+echo "Setting X11 keyboard layouts"
+localectl --no-convert set-x11-keymap us,us microsoft4000, colemak, caps:escape_shifted_capslock compose:ralt
+
+# set the root password
+echo "Please set the root password"
+passwd
+
+# create a new user with sudo privileges
+echo "Creating user $USER"
+useradd -m "$USER"
+pacman -S sudo
+echo "$USER ALL=(ALL) ALL" > "/etc/sudoers.d/01_$USER"
+echo "Please set the password for user $USER"
+passwd "$USER"
+
+echo "Exiting chroot"
---
+++ install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# First script to run to install Arch.
+#
+# Prerequisites:
+#   - be booted in UEFI mose
+#   - be connected to the internet
+
+# variables
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "USAGE: ./install.sh [device name] [default username]"
+    exit 1
+fi
+DEV="$1"
+USER="$2"
+
+# make sure you're booted in UEFI mode
+echo "Verifying UEFI mode"
+if ! ls /sys/firmware/efi/efivars; then
+    echo "You are not booted in UEFI mode. Aborting."
+    exit 2
+fi
+
+# verify Internet connection
+echo "Verifying Internet connection"
+if ! ping -c 5 archlinux.org; then
+    echo "You are not connected to the Internet. Aborting."
+    exit 3
+fi
+
+# update system clock
+echo "Updating system clock"
+timedatectl set-ntp true
+
+# create partitions
+echo "Creating partitions on /dev/$DEV"
+parted /dev/"$DEV" mklabel gpt
+parted /dev/"$DEV" mkpart primary fat32 1MiB 261MiB
+parted /dev/"$DEV" set 1 esp on
+parted /dev/"$DEV" mkpart primary ext4 261MiB 33029MiB
+parted /dev/"$DEV" mkpart primary ext4 33029MiB 41221MiB
+parted /dev/"$DEV" mkpart primary ext4 41221MiB 100%
+
+# format partitions
+echo "Formatting partitions"
+mkfs.fat -F32 /dev/"$DEV"1
+mkfs.ext4 /dev/"$DEV"2
+mkswap /dev/"$DEV"3
+mkfs.ext4 /dev/"$DEV"4
+
+# mount partitions
+echo "Mounting partitions"
+mount /dev/"$DEV"2 /mnt
+mkdir /mnt/efi /mnt/home
+mount /dev/"$DEV"1 /mnt/efi
+mount /dev/"$DEV"4 /mnt/home
+swapon /dev/"$DEV"3
+
+# adjust mirrors
+echo "Setting desired mirrors"
+cp mirrorlist /etc/pacman.d/mirrorlist
+
+# install base system
+echo "Installing base system"
+pacstrap /mnt base linux linux-firmware
+
+# generate fstab
+echo "Generating fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# run chroot.sh from new root
+echo "chroot-ing to new root"
+arch-chroot /mnt ./chroot.sh "$USER"
+
+# reboot
+echo "All done! Your system will now reboot."
+read -p "Press enter to continue..."
+reboot
