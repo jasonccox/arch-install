29d14d8a14d394c98f4c131b0fa44a6bfcb3db1d
Jason Cox <dev@jasoncarloscox.com>
2019-11-25 15:14:07 -0700

    add dotfiles

--- first-boot.sh
+++ first-boot.sh
@@ -5,3 +5,8 @@
 # Keyboard
 echo "Setting X11 keyboard layouts"
 localectl --no-convert set-x11-keymap us,us microsoft4000, colemak, caps:escape_shifted_capslock,compose:ralt
+
+# Dotfiles
+echo "Setting up config files"
+git clone https://gitlab.com/jasonccox/dotfiles.git
+./dotfiles/setup.sh
