610ad4aace7781fb03c4436580cdf731f27d2bab
Jason Cox <dev@jasoncarloscox.com>
2020-02-28 20:58:03 -0700

    add bash-completion and pkgfile

--- chroot.sh
+++ chroot.sh
@@ -81,7 +81,10 @@ pacman -S --noconfirm phonon-qt5-vlc plasma-meta plasma-nm sddm-kcm kde-gtk-conf
 # Other Packages
 echo "Installing additional software"
 pacman -Rs --noconfirm vim
-pacman -S --noconfirm base-devel konsole firefox gvim zip unzip openssh hunspell-en_US hunspell-es_any nextcloud-client pulseaudio-alsa pulseaudio-bluetooth dosfstools e2fsprogs nodejs npm tmux man-pages moreutils xclip khal todoman vdirsyncer python-keyring python-dbus kwallet
+pacman -S --noconfirm base-devel konsole firefox gvim zip unzip openssh hunspell-en_US hunspell-es_any nextcloud-client pulseaudio-alsa pulseaudio-bluetooth dosfstools e2fsprogs nodejs npm tmux man-pages moreutils xclip khal todoman vdirsyncer python-keyring python-dbus kwallet bash-completion pkgfile
+
+# enable pkgfile automatic updates
+systemctl enable pkgfile-update.timer
 
 # set the root password
 echo "Please set the root password"
